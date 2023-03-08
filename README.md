1514f66dbeeb9ce4aa2d705a8678117fd58a4845
Martino Giorgi <martino.giorgi@brightside-capital.com>
2023-03-08 15:17:06 +0100

    fix david

---
+++ README.md
@@ -0,0 +1,3 @@
+# Install packages
+
+Run: `pip install -r requirements.txt`
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-siu
+import os
+
+import numpy as np
+from keras import Sequential
+from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
+from keras.layers import Dense, Conv2D, MaxPooling2D, AveragePooling2D, Flatten
+from tensorflow import optimizers
+
