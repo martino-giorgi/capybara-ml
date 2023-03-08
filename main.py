d8fc092d862759c9853212fbfe8a079529da625f
David Ferré López <david@dyn-42-214.mobile.unibas.ch>
2023-03-08 15:51:11 +0100

    guapa

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ from keras.callbacks import EarlyStopping
 from keras.layers import Dense, Conv2D, MaxPooling2D, AveragePooling2D, Flatten
 from tensorflow import optimizers
 
+# model siu
+for i in i do so if not then is a hand, thats all 
