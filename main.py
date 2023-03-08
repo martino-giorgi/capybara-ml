618735549aa75bda0968d3eb9447b85045ed2713
silva <silva>
2023-03-08 15:59:09 +0100

    siuuuuuuuuuuuum

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ from keras.layers import Dense, Conv2D, MaxPooling2D, AveragePooling2D, Flatten
 from tensorflow import optimizers
 
 # model siu
-for i in i do so if not then is a hand, thats all 
+for i in i do so if not then is a hand, thats all
+    for n in siu:
+        i = 'True'
+        t = 'False'
